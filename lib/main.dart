c96bdc58cec09525e84fef508483cfc88f6b7916
Yousefelzegheby <Yousefelzegheby>
2024-02-25 18:10:53 +0200

    cubit main

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class GraduationProgect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return BlocProvider(
       create: (context) => LogInCubit(),
       child: MaterialApp.router(
@@ -26,18 +25,6 @@ class GraduationProgect extends StatelessWidget {
             )).copyWith(scaffoldBackgroundColor: AppColors.kLightColor),
         routerConfig: AppRoutes.router,
       ),
-=======
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-          appBarTheme: const AppBarTheme(
-        color: AppColors.kLightColor,
-        elevation: 0,
-      )).copyWith(scaffoldBackgroundColor: AppColors.kLightColor),
-      routerConfig: AppRoutes.router,
-      // hello =========>
->>>>>>> c60b30f71ffb2048ec3e109658a5d3d477236ddc
     );
   }
 }
